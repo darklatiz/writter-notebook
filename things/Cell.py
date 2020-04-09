57b51b0344b076723ca8668763915e6e073212f9
Spike Storm <software.j2ee@gmail.com>
2020-04-08 19:32:31 -0700

    addin QLine oibject to grid

--- things/Cell.py
+++ things/Cell.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QLine
 from PyQt5.QtGui import QBrush
 
 
@@ -14,6 +14,36 @@ class Cell:
             "top": True,
             "bottom": True
         }
+
+    def init_grid(self, painter, w):
+        x = self.row * w
+        y = self.col * w
+
+        # top
+        line_top = QLine(x, y, x + w, y)
+        painter.drawLine(line_top)
+        self.walls["top"] = line_top
+
+        # right
+        line_right = QLine(x + w, y, x + w, y + w)
+        painter.drawLine(line_right)
+        self.walls["right"] = line_right
+
+        # bottom
+        line_bottom = QLine(x + w, y + w, x, y + w)
+        painter.drawLine(line_bottom)
+        self.walls["bottom"] = line_bottom
+
+        # left
+        line_left = QLine(x, y + w, x, y)
+        painter.drawLine(line_left)
+        self.walls["left"] = line_left
+
+        if self.visited:
+            painter.setBrush(QBrush(Qt.darkMagenta, Qt.SolidPattern))
+            painter.drawRect(self.row * w, self.col * w, w, w)
+
+
 
     def show(self, painter, w):
         x = self.row * w
@@ -37,3 +67,6 @@ class Cell:
         if self.visited:
             painter.setBrush(QBrush(Qt.darkMagenta, Qt.SolidPattern))
             painter.drawRect(self.row * w, self.col * w, w, w)
+
+    def check_neighbours(self, top, right, bottom, left):
+        neighbours = []
--- ui/maze.py
+++ ui/maze.py
@@ -4,29 +4,34 @@ from PyQt5.QtGui import QPainter, QBrush, QPen
 from PyQt5.QtCore import Qt
 import math
 from things.Cell import Cell
+from collections import  deque
 import sys
 
 class Window(QMainWindow):
     '''
         Recursive BAckTacker
     '''
-    def __init__(self):
+    def __init__(self, width=800, height=800, top=150, left=150):
         super().__init__()
         self.title = "PyQt5 Drawing Rectangle"
-        self.top = 100
-        self.left = 100
-        self.width = 800
-        self.height = 800
+        self.top = top
+        self.left = left
+        self.width = width
+        self.height = height
         self.InitWindow()
         self.grid = []
         self.w = 40
         self.rows = math.floor(self.width/self.w)
         self.columns = math.floor(self.width/self.w)
+        self.myStaxk = deque()
         for i in range(self.rows):
+            row = []
             for j in range(self.columns):
-                self.grid.append(Cell(i,j))
-        self.current = self.grid[0]
+                row.append(Cell(i,j))
+            self.grid.append(row)
+        self.current = self.grid[0][0]
         self.current.visited = True
+        self.myStaxk.append(self.grid[0][0])
 
     def InitWindow(self):
         self.setWindowIcon(QtGui.QIcon("icon.png"))
@@ -38,13 +43,17 @@ class Window(QMainWindow):
     def paintEvent(self, e):
         painter = QPainter(self)
         painter.setPen(QPen(Qt.black, 1, Qt.SolidLine))
-        #(int x, int y, int width, int height)
-        for cell in self.grid:
-            #painter.drawRect(cell.row * self.w, cell.col * self.w, self.w, self.w)
-            cell.show(painter, self.w)
+        for cell_list in self.grid:
+            for cell in cell_list:
+                cell.init_grid(painter, self.w)
+
+        while len(self.myStaxk) > 0:
+            ccell = self.myStaxk.pop()
+            self.current = ccell;
+
 
 
 
 App = QApplication(sys.argv)
-window = Window()
+window = Window(600,600)
 sys.exit(App.exec())
